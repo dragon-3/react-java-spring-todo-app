477cb3faaa9b697e7e193052a2fdbdd93ea9d512
Sean Narron <seannarron9@gmail.com>
2021-08-17 17:08:07 +0900

    Added state and handle change

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -10,17 +10,44 @@ class TodoApp extends Component {
     }
 }
 
+
+
 class LoginComponent extends Component {
-        render() {
-            return(
-                <div>
-                    User Name: <input type="text" name="username"/>
-                    Password: <input type="password" name="password"/>
-                    <button>Login</button>
-                </div>
-                
-            )
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: 'dragon3',
+            password: ''
         }
+        this.handleUsernameChange = this.handleUsernameChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    }
+
+    handleUsernameChange(event) {
+        console.log(event.target.value);
+        this.setState({
+            username: event.target.value
+        })
+    }
+
+    handlePasswordChange(event) {
+        console.log(event.target.value);
+        this.setState({
+            password: event.target.value
+        })
+    }
+
+    render() {
+        return(
+            <div>
+                User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleUsernameChange}/>
+                Password: <input type="password" name="password" value={this.state.password} onChange={this.handlePasswordChange}/>
+                <button>Login</button>
+            </div>
+            
+        )
+    }
 }
 
 export default TodoApp;
