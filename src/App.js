9766c079994dbc8ecc35ab92d4a37ea52380cfa8
Sean Narron <seannarron9@gmail.com>
2021-08-18 13:56:22 +0900

    Added bootstrap to project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './bootstrap.css';
 import Counter from './components/counter/Counter';
 import TodoApp from './components/todo/TodoApp';
 
