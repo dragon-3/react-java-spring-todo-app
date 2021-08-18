9ce810f30fb33c01332870c24df1fadf09ed0299
Sean Narron <seannarron9@gmail.com>
2021-08-18 10:07:58 +0900

    Added router path

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,12 +1,28 @@
 import React, {Component} from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 class TodoApp extends Component {
     render() {
         return (
             <div className="TodoApp">
-                <LoginComponent />
+                <Router>
+                    <>
+                        <Route path="/" exact component={LoginComponent} />
+                        <Route path="/login" component={LoginComponent} />
+                        <Route path="/welcome" component={WelcomeComponent}/>
+                    </>
+                </Router>
+                {/* <LoginComponent />
+                <WelcomeComponent /> */}
             </div>
         )
+    }
+}
+
+class WelcomeComponent extends Component {
+    render() {
+        return <div>Welcome, Dragon3</div>
+
     }
 }
 
