61080312e91436e8474fd59810c8872432b3c5e2
Sean Narron <seannarron9@gmail.com>
2021-08-18 16:53:59 +0900

    Added unique key

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -108,7 +108,7 @@ class ListTodosComponent extends Component {
                             {
                                 this.state.todos.map(
                                     todo =>
-                                    <tr>
+                                    <tr key={todo.id}>
                                         <td>{todo.description}</td>
                                         <td>{todo.done.toString()}</td>
                                         <td>{todo.targetDate.toString()}</td>
