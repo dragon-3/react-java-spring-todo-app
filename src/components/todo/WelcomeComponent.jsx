21fd10270a2e95e3adf7c0fd8e844813b6e5f4a4
Sean Narron <seannarron9@gmail.com>
2021-08-19 17:11:33 +0900

    Created a function for displaying content from api

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
+import HelloWorldService from '../../api/todo/HelloWorldService'
 
 
 class WelcomeComponent extends Component {
@@ -7,6 +8,10 @@ class WelcomeComponent extends Component {
     constructor(props) {
         super(props)
         this.retriveWelcomeMessage = this.retriveWelcomeMessage.bind(this)
+        this.handleSuccefulResponse = this.handleSuccefulResponse.bind(this)
+        this.state ={
+            welcomeMessage: ''
+        }
     }
 
     render() {
@@ -17,15 +22,28 @@ class WelcomeComponent extends Component {
                     Welcome {this.props.match.params.name}. 
                     You can manage your todos <Link to="/todos">here</Link>.
                 </div>
+
                 <div className="container">
                     Click here to get a message
                     <button onClick={this.retriveWelcomeMessage} className="btn btn-success">Welcome message</button>
+                </div>
+
+                <div className="container">
+                    {this.state.welcomeMessage}
                 </div>
             </>
         )
     }
     retriveWelcomeMessage() {
-        console.log('retrived click')
+        HelloWorldService.executeHelloWorldService()
+        .then(response =>this.handleSuccefulResponse(response))
+        //.catch()
+    }
+
+    handleSuccefulResponse(response) {
+        this.setState({
+            welcomeMessage: response.data
+        })
     }
 }
 
