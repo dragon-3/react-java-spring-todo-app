3b6f77bec0adcfaa45f16360347bbd9ab2b551a8
Sean Narron <seannarron9@gmail.com>
2021-08-19 15:56:13 +0900

    Added a welcome button

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -3,15 +3,29 @@ import {Link} from 'react-router-dom'
 
 
 class WelcomeComponent extends Component {
+
+    constructor(props) {
+        super(props)
+        this.retriveWelcomeMessage = this.retriveWelcomeMessage.bind(this)
+    }
+
     render() {
         return (
             <>
                 <h1>Welcome!</h1>
                 <div className="container">
-                    Welcome {this.props.match.params.name}. You can manage your todos <Link to="/todos">here</Link>.
+                    Welcome {this.props.match.params.name}. 
+                    You can manage your todos <Link to="/todos">here</Link>.
+                </div>
+                <div className="container">
+                    Click here to get a message
+                    <button onClick={this.retriveWelcomeMessage} className="btn btn-success">Welcome message</button>
                 </div>
             </>
         )
+    }
+    retriveWelcomeMessage() {
+        console.log('retrived click')
     }
 }
 
