0fa1382f6ec12bdb9b1dd383a51a66fc07b653d4
Sean Narron <seannarron9@gmail.com>
2021-08-23 13:18:25 +0900

    Added api for retriving a todo

--- src/api/todo/TodoDataService.js
+++ src/api/todo/TodoDataService.js
@@ -8,10 +8,17 @@ class TodoDataService {
         return axios.get(`http://localhost:8082/users/${name}/todos`)
     }
 
+    retriveTodo(name, id) {
+        return axios.get(`http://localhost:8082/users/${name}/todos/${id}`)
+
+    }
+
     deleteTodo(name, id) {
         return axios.delete(`http://localhost:8082/users/${name}/todos/${id}`)
 
     }
+
+    
 }
 
 export default new TodoDataService()
