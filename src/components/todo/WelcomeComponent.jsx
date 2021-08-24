caf9108ef3ee600154449fc300a37f2c1c055fb7
Sean Narron <seannarron9@gmail.com>
2021-08-24 15:58:55 +0900

    Modified error message

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -52,6 +52,7 @@ class WelcomeComponent extends Component {
 
     handleSuccefulResponse(response) {
         console.log(response)
+
         this.setState({
             welcomeMessage: response.data.message
         })
@@ -59,8 +60,17 @@ class WelcomeComponent extends Component {
 
     handleError(error) {
         console.log(error.response)
+        let errorMessage = '';
+
+        if(error.message) {
+            errorMessage += error.message
+        }
+
+        if(error.response && error.response.data) {
+            errorMessage += error.response.data.message
+        }
          this.setState({
-            welcomeMessage: error.response.data.message
+            welcomeMessage: errorMessage
         }) 
     }
 }
